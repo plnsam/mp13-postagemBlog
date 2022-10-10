8f77b993e3bb3474c08ceea27dce6fa270473d84
plnsam <sammillsswan97@gmail.com>
2022-10-10 13:05:26 -0300

    last commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let blogObj = {
+  titulo: "titulo",
+  mensagem: "esta é a mensagem",
+  autor: "eu sou o autor",
+  view: 3000,
+  comentarios: [
+    { autor: "eu sou o autor", mensagem: "esta é a mensagem" },
+    { autor: "eu sou o autor", mensagem: "esta é a mensagem" },
+  ],
+  estaAoVivo: true,
+};
+
+console.log(blogObj);
